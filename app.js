6c85feffb7ebbfbbb4c887f2e4782adccb5b8808
DannyHucc <dannyhu10011101@gmail.com>
2023-05-15 20:50:48 +0800

    feat: add create shortURL function

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const exphbs = require('express-handlebars')
+const bodyParser = require('body-parser')
 const routes = require('./routes')
 require('./config/mongoose')
 
@@ -8,6 +9,7 @@ const port = process.env.PORT || 3000
 
 app.engine('hbs', exphbs({ defaultLayout: 'main', extname: '.hbs' }))
 app.set('view engine', 'hbs')
+app.use(bodyParser.urlencoded({ extended: true }))
 app.use(routes)
 
 app.listen(port, () => {
--- routes/modules/home.js
+++ routes/modules/home.js
@@ -1,8 +1,46 @@
 const express = require('express')
 const router = express.Router()
+const URL = require('models/shortenURL')
+const generateShortURL = require('utils/shortenURL_generator')
+
+const port = process.env.PORT || 3000
+const server = `http://localhost:${port}/`
 
 router.get('/', (req, res) => {
     res.render('index')
+})
+
+router.post('/', (req, res) => {
+    const { originalURL } = req.body
+
+    function checkPath() {
+        const path = generateShortURL()
+        const shortURL = server + path
+
+        URL.exists({ shortURL })
+            .then((exist) => {
+                if (exist) {
+                    checkPath()
+                } else {
+                    URL.findOne({ originalURL })
+                        .then((URLData) => {
+                            if (URLData) {
+                                console.log(`Have URL: res.render('index')`)
+                            } else {
+                                URL.create({ originalURL, shortURL })
+                                    .then(() => {
+                                        console.log(`Create URL: res.render('index')`)
+                                    })
+                                    .catch((err) => console.log(err))
+                            }
+                        })
+                        .catch((err) => console.log(err))
+                }
+            })
+            .catch((err) => console.log(err))
+    }
+
+    checkPath()
 })
 
 module.exports = router
